9af4e3578c3967790dfbd7d640bcb7cb802929ee
Aditya2487 <73511460+Aditya2487@users.noreply.github.com>
2026-01-03 22:28:56 +0530

    first commit

--- basics.cpp
+++ basics.cpp
@@ -1,8 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main(){
-    int a,b;    
-    cin>>a>>b;
-    cout<<a+b;
+    //range -> [-10^9, 10^9]
+    int a,b;
+    cin >> a >> b;
+    cout << a + b << endl;
+
+    int numInt = INT_MAX; // Maximum value for an integer
+    cout << "The maximum integer value is: " << numInt << endl;
+    //[10^12]
+    long numLongInt = LONG_MAX; // Maximum value for a long integer 
+    cout << "The maximum long integer value is: " << numLongInt << endl;
+    // [10^18]
+    long long numLong = LLONG_MAX; // Maximum value for a long long integer
+    cout << "The maximum long long integer value is: " << numLong << endl;
+
     return 0;
 }
