c9038f0b70e308bc007295ece3185a2e4b81ba96
Aditya2487 <73511460+Aditya2487@users.noreply.github.com>
2026-01-14 16:15:51 +0530

    14th Jan 2026

---
+++ Python/python_basics.py
@@ -0,0 +1,13 @@
+print("My name is Python")
+print("I am learning programming")
+print(2 + 3)
+print("2 + 3 =", 2 + 3)
+name="Aditya"
+age = 24
+city = "Bihar"
+print(name)
+print(len(name))
+print("My name is", name)
+print(name, "is", age, "years old")
+print(name, "lives in", city)
+print(f"{name} is {age} years old and lives in {city}")
